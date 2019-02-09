57ac6fa1ea4c75da7310c592f9cc247e9375c710
Nick Morrow <nmorrow11@gmail.com>
2019-02-08 16:58:04 -0800

    connect to nhl api and getting all teams

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,26 +1,36 @@
 import React from 'react';
+import axios from 'axios';
 
 class Search extends React.Component {
 
 	constructor(props){
 		super(props);
 		this.state = {
-			query: ''
+			query: '',
+			teams: {},
 		};
 		this.updateQuery = this.updateQuery.bind(this);
 		this.submitQuery = this.submitQuery.bind(this);
 	}
 
-	updateQuery(event) {
+	componentDidMount() {
 
-		this.setState({query: event.target.value})
+		axios.get('https://statsapi.web.nhl.com/api/v1/teams')
+				.then(res => {
+					const teams = res.data.teams;
+					this.setState({ teams })
+				})
+	}
+
+	updateQuery(value) {
+		console.log(value)
+		this.setState({query: value})}
 	}
 	submitQuery(event) {
 
 		if(event.key == "Enter"){
-
 		} else {
-			this.updateQuery(event)
+			this.updateQuery(event.target.value)
 		}
 	}
 	render(){
